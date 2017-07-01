deda1965dc8020fcdc4bc9d9a892131ff7f4d320
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2017-07-01 20:52:57 +0200

    wifi and mqtt

---
+++ espthp.ino
@@ -0,0 +1,73 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "config.h"
+
+WiFiClient WiFiClient;
+PubSubClient client(WiFiClient);
+
+#define STATUS_DISCONNECTED "disconnected"
+#define STATUS_ONLINE "online"
+
+const String chipId = String(ESP.getChipId());
+const String baseTopic = "raw/" + chipId + "/";
+const char* tempTopic = String(baseTopic + "temperature").c_str();
+const char* humiTopic = String(baseTopic + "humidity").c_str();
+const char* presTopic = String(baseTopic + "pressure").c_str();;
+const char* willTopic = String(baseTopic + "status").c_str();
+
+
+void setup() {
+  
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP()); 
+}
+
+void loop() {
+  yield();
+  if (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    if (client.connect(chipId.c_str(), willTopic, 1, true, STATUS_DISCONNECTED)) {
+      Serial.println("MQTT client connected.");
+      client.publish(willTopic, STATUS_ONLINE, true);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" will try again in 5 seconds");      
+      delay(5000);    
+    }
+  }
+
+  if (client.connected()) {
+    bmeReadSend();
+    client.loop();
+    delay(10000);
+  }
+}
+
+void bmeReadSend() {
+  Serial.println("Sending values");    
+  int t = 20; 
+  int h = 50;
+  int p = 1014;  
+  client.publish(tempTopic, String(t).c_str());
+  client.publish(humiTopic, String(h).c_str());
+  client.publish(presTopic, String(p).c_str());
+}
+
